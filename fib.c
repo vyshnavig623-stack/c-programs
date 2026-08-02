674fb1a83e2f83d91cccf925c16b44c12a9d6f9c
vyshnavig623-stack <vyshnavig623@gmail.com>
2026-08-02 13:51:34 +0530

    Add files via upload
    fibonacci series

---
+++ fib.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter number of terms:");
+    scanf("%d",&n);
+    int a=0,b=1,c;
+    for(int i=1;i<=n;i++){
+        printf("%d",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
