2ee9a022e8d52e6cfe8b8988d342ab80d24c7b10
vyshnavig623-stack <vyshnavig623@gmail.com>
2026-08-06 21:47:18 +0530

    factorial usin recursion

---
+++ factf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int factorial(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+int main(){
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    printf("factorial of number :%d",factorial(n));
+    return 0;
+}
